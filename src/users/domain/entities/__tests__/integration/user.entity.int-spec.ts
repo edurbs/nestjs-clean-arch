64428b9c4f2b01d24662512e968b5ffd1582dffa
Eduardo Soares <edurbs@gmail.com>
2023-11-25 20:04:48 -0300

    add validate email tests

--- src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
@@ -3,26 +3,67 @@ import { UserDataBuilder } from '../../../testing/helpers/user-data-builders'
 import { UserEntity, UserProps } from '../../user.entity'
 describe('UserEntity integration tests', () => {
   describe('Constructor method', () => {
-    it('should throw an error when creating a user with null name', () => {
-      const props: UserProps = {
-        ...UserDataBuilder({}),
-        name: null,
-      }
-      expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+    describe('Name field', () => {
+      it('should throw an error when creating a user with null name', () => {
+        const props: UserProps = {
+          ...UserDataBuilder({}),
+          name: null,
+        }
+        expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+      })
+      it('should throw an error when creating a user with empty name', () => {
+        const props: UserProps = {
+          ...UserDataBuilder({}),
+          name: '',
+        }
+        expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+      })
+      it('should throw an error when creating a user with name with more then 255 caracters', () => {
+        const props: UserProps = {
+          ...UserDataBuilder({}),
+          name: 'a'.repeat(256),
+        }
+        expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+      })
+
+      it('should throw an error when creating a user with name with more then 255 caracters', () => {
+        const props: UserProps = {
+          ...UserDataBuilder({}),
+          name: 10 as any,
+        }
+        expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+      })
     })
-    it('should throw an error when creating a user with empty name', () => {
-      const props: UserProps = {
-        ...UserDataBuilder({}),
-        name: '',
-      }
-      expect(() => new UserEntity(props)).toThrow(EntityValidationError)
-    })
-    it('should throw an error when creating a user with name with more then 255 caracters', () => {
-      const props: UserProps = {
-        ...UserDataBuilder({}),
-        name: 'a'.repeat(256),
-      }
-      expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+    describe('Email field', () => {
+      it('should throw an error when creating a user with null email', () => {
+        const props: UserProps = {
+          ...UserDataBuilder({}),
+          email: null,
+        }
+        expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+      })
+      it('should throw an error when creating a user with empty email', () => {
+        const props: UserProps = {
+          ...UserDataBuilder({}),
+          email: '',
+        }
+        expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+      })
+      it('should throw an error when creating a user with email with more then 255 caracters', () => {
+        const props: UserProps = {
+          ...UserDataBuilder({}),
+          email: 'a'.repeat(256),
+        }
+        expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+      })
+
+      it('should throw an error when creating a user with email with more then 255 caracters', () => {
+        const props: UserProps = {
+          ...UserDataBuilder({}),
+          email: 10 as any,
+        }
+        expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+      })
     })
   })
 })
