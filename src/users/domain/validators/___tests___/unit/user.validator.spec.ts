24781c8b60e712a49f6c3389ec0b322f2f87605a
Eduardo Soares <edurbs@gmail.com>
2023-11-25 19:36:21 -0300

    add createdAt validation tests

--- src/users/domain/validators/___tests___/unit/user.validator.spec.ts
+++ src/users/domain/validators/___tests___/unit/user.validator.spec.ts
@@ -1,3 +1,4 @@
+import { UserProps } from '@/users/domain/entities/user.entity'
 import { UserDataBuilder } from '../../../testing/helpers/user-data-builders'
 import {
   UserRules,
@@ -6,16 +7,17 @@ import {
 } from '../../user.validator'
 
 let sut: UserValidator
+let props: UserProps
 
 describe('User validator unit tests', () => {
   beforeEach(() => {
     sut = UserValidatorFactory.create()
+    props = {
+      ...UserDataBuilder({}),
+    }
   })
 
   it('Shoud validate with valid fields', () => {
-    const props = {
-      ...UserDataBuilder({}),
-    }
     const isValid = sut.validate(props)
     expect(isValid).toBeTruthy()
     expect(sut.validatedData).toStrictEqual(new UserRules(props))
@@ -163,6 +165,24 @@ describe('User validator unit tests', () => {
       expect(isValid).toBeFalsy()
       expect(sut.errors['password']).toStrictEqual([
         'password must be shorter than or equal to 100 characters',
+      ])
+    })
+  })
+
+  describe('CreatedAt field', () => {
+    it('Should not validate with all fields null', () => {
+      const isValid = sut.validate({ ...props, createdAt: 10 as any })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['createdAt']).toStrictEqual([
+        'createdAt must be a Date instance',
+      ])
+    })
+
+    it('Should not validate with null', () => {
+      const isValid = sut.validate({ ...props, createdAt: '2023' as any })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['createdAt']).toStrictEqual([
+        'createdAt must be a Date instance',
       ])
     })
   })
