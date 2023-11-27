fc2d0eb137aa062dbf702f582999f2e24ec41e14
Eduardo Soares <edurbs@gmail.com>
2023-11-27 19:30:07 -0300

    add filter method tests

--- src/shared/domain/repositories/___tests___/unit/in-memory-searcheable.repository.spec.ts
+++ src/shared/domain/repositories/___tests___/unit/in-memory-searcheable.repository.spec.ts
@@ -28,13 +28,52 @@ describe('In memory repository unit tests', () => {
   beforeEach(() => {
     sut = new StubInMemorySearcheableRepository()
     entity = new StubEntity({
-      name: 'test',
+      name: 'some name',
       price: 10,
     })
   })
 
   describe('applyFilter method', () => {
-    it('should return all items when filter is null', () => {})
+    let itemsToFilter: StubEntity[]
+    let spyFilterMethod: jest.SpyInstance
+    beforeEach(() => {
+      const entity1 = new StubEntity({
+        name: 'some name',
+        price: 10,
+      })
+      const entity2 = new StubEntity({
+        name: 'SOME NAME',
+        price: 10,
+      })
+      const entity3 = new StubEntity({
+        name: 'fake',
+        price: 10,
+      })
+      itemsToFilter = [entity1, entity2, entity3]
+      spyFilterMethod = jest.spyOn(itemsToFilter, 'filter')
+    })
+
+    async function doFilter(filter: string | null) {
+      return await sut['applyFilter'](itemsToFilter, filter)
+    }
+
+    it('should return all items when filter is null', async () => {
+      expect(await doFilter(null)).toStrictEqual(itemsToFilter)
+      expect(spyFilterMethod).not.toHaveBeenCalled()
+    })
+
+    it('should return filtered items when filter use upper and lower case', async () => {
+      expect(await doFilter('SOME name')).toStrictEqual([
+        itemsToFilter[0],
+        itemsToFilter[1],
+      ])
+      expect(spyFilterMethod).toHaveBeenCalledTimes(1)
+    })
+
+    it('should return nothing when filter dont match anything', async () => {
+      expect(await doFilter('no-filter')).toHaveLength(0)
+      expect(spyFilterMethod).toHaveBeenCalledTimes(1)
+    })
   })
 
   describe('applySort method', () => {})
