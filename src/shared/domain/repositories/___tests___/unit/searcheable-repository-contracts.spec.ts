e9397f458fbed1bd0981db1eff47facf204eac22
Eduardo Soares <edurbs@gmail.com>
2023-11-27 15:37:13 -0300

    add tests

--- src/shared/domain/repositories/___tests___/unit/searcheable-repository-contracts.spec.ts
+++ src/shared/domain/repositories/___tests___/unit/searcheable-repository-contracts.spec.ts
@@ -2,47 +2,101 @@ import { SearchParams } from '../../searcheable-repository-contracts'
 
 describe('Searcheable repository unit tests', () => {
   describe('SearchParams tests', () => {
-    const pageParams = [{ page: null }, { page: 0 }, { page: -1 }, { page: 1 }]
+    const pageParams: { page: number }[] = [
+      { page: 0 },
+      { page: -1 },
+      { page: 1 },
+    ]
 
-    it('Should set page property with 1 when no value is provided', () => {
-      const sut = new SearchParams()
-      expect(sut.page).toBe(1)
-    })
+    describe('Page property', () => {
+      it('Should set page property with 1 when no value is provided', () => {
+        const sut = new SearchParams()
+        expect(sut.page).toBe(1)
+      })
 
-    it('should set page property when a valid is provided', () => {
-      const sut = new SearchParams({ page: 2 })
-      expect(sut.page).toBe(2)
-    })
+      it('should set page property when a valid is provided', () => {
+        const sut = new SearchParams({ page: 2 })
+        expect(sut.page).toBe(2)
+      })
 
-    it('should set page property with integer number when a float number is provided', () => {
-      const sut = new SearchParams({ page: 5.5 })
-      expect(sut.page).toBe(5)
-    })
+      it('should set page property with integer number when a float number is provided', () => {
+        const sut = new SearchParams({ page: 5.5 })
+        expect(sut.page).toBe(5)
+      })
 
-    it('Should set page property with provided value', () => {
-      pageParams.forEach(i => {
-        expect(new SearchParams({ page: i.page }).page).toEqual(1)
+      it('Should set page property with provided value', () => {
+        pageParams.forEach(i => {
+          expect(new SearchParams({ page: i.page }).page).toEqual(1)
+        })
       })
     })
 
-    it('Should set perPage property with 15 when no value is provided', () => {
-      const sut = new SearchParams()
-      expect(sut.perPage).toBe(15)
+    describe('perPage property', () => {
+      it('Should set perPage property with 15 when no value is provided', () => {
+        const sut = new SearchParams()
+        expect(sut.perPage).toBe(15)
+      })
+
+      it('should set perPage property with integer number when a float number is provided', () => {
+        const sut = new SearchParams({ perPage: 5.5 })
+        expect(sut.perPage).toBe(5)
+      })
+
+      it('should set perPage property when a valid is provided', () => {
+        const sut = new SearchParams({ perPage: 25 })
+        expect(sut.perPage).toBe(25)
+      })
+
+      it('Should set perPage property with provided value', () => {
+        pageParams.forEach(i => {
+          expect(new SearchParams({ page: i.page }).perPage).toEqual(15)
+        })
+      })
     })
 
-    it('should set perPage property with integer number when a float number is provided', () => {
-      const sut = new SearchParams({ perPage: 5.5 })
-      expect(sut.perPage).toBe(5)
+    describe('sort property', () => {
+      it('Should set sort property with null when no value is provided', () => {
+        const sut = new SearchParams()
+        expect(sut.sort).toBeNull()
+      })
+      it('Should set sort property with null when empty value is provided', () => {
+        const sut = new SearchParams({ sort: '' })
+        expect(sut.sort).toBeNull()
+      })
+      it('should set sort property when a valid is provided', () => {
+        const sut = new SearchParams({ sort: 'name' })
+        expect(sut.sort).toBe('name')
+      })
     })
+    describe('sortDir property', () => {
+      it('Should set sortDir property with null when no value is provided', () => {
+        const sut = new SearchParams()
+        expect(sut.sortDir).toBeNull()
+      })
 
-    it('should set perPage property when a valid is provided', () => {
-      const sut = new SearchParams({ perPage: 25 })
-      expect(sut.perPage).toBe(25)
+      it('should set sortDir property with desc when desc is provided', () => {
+        const sut = new SearchParams({ sort: 'someValue', sortDir: 'desc' })
+        expect(sut.sortDir).toBe('desc')
+      })
+      it('should set sortDir property with asc when asc is provided', () => {
+        const sut = new SearchParams({ sort: 'someValue', sortDir: 'asc' })
+        expect(sut.sortDir).toBe('asc')
+      })
+
+      it('should set sortDir property with null when sort property if not provided ', () => {
+        const sut = new SearchParams({ sortDir: 'asc' })
+        expect(sut.sortDir).toBeNull()
+      })
     })
+    describe('filter property', () => {
+      it('Should set filter property with null when no value is provided', () => {
+        const sut = new SearchParams()
+        expect(sut.filter).toBeNull()
+      })
 
-    it('Should set perPage property with provided value', () => {
-      pageParams.forEach(i => {
-        expect(new SearchParams({ page: i.page }).perPage).toEqual(15)
+      it('should set filter property when a valid value is provided', () => {
+        const sut = new SearchParams({ filter: 'someValue' })
+        expect(sut.filter).toBe('someValue')
       })
     })
   })
--- src/shared/domain/repositories/searcheable-repository-contracts.ts
+++ src/shared/domain/repositories/searcheable-repository-contracts.ts
@@ -12,18 +12,18 @@ export type SearchProps<Filter = string> = {
 }
 
 export class SearchParams {
-  protected _page: number
-  protected _perPage = 15
-  protected _sort: string | null
-  protected _sortDir: SortDirection | null
-  protected _filter: string | null
+  private _page: number
+  private _perPage: number
+  private _sort: string | null
+  private _sortDir: SortDirection | null
+  private _filter: string | null
 
   constructor(props: SearchProps = {}) {
-    this.page = props.page
-    this.perPage = props.perPage
-    this.sort = props.sort
-    this.sortDir = props.sortDir
-    this.filter = props.filter
+    this.page = props.page || 1
+    this.perPage = props.perPage || 15
+    this.sort = props.sort || null
+    this.sortDir = props.sortDir || null
+    this.filter = props.filter || null
   }
 
   get page(): number {
@@ -31,11 +31,7 @@ export class SearchParams {
   }
 
   private set page(value: number) {
-    let page = value
-    if (page <= 0 || !page) {
-      page = 1
-    }
-    this._page = Math.trunc(page)
+    this._page = value <= 0 || !value ? 1 : Math.trunc(value)
   }
 
   get perPage(): number {
@@ -43,11 +39,7 @@ export class SearchParams {
   }
 
   private set perPage(value: number) {
-    let perPage = value
-    if (perPage <= 0 || !perPage) {
-      perPage = this._perPage
-    }
-    this._perPage = Math.trunc(perPage)
+    this._perPage = value <= 0 || !value ? this._perPage : Math.trunc(value)
   }
 
   get sort(): string | null {
@@ -55,20 +47,20 @@ export class SearchParams {
   }
 
   private set sort(value: string | null) {
-    this._sort = value === null || value === '' ? null : `${value}`
+    this._sort = !value ? null : value
   }
 
-  get sortDir(): SortDirection | null {
+  get sortDir(): string | null {
     return this._sortDir
   }
 
-  private set sortDir(value: SortDirection | null) {
-    if (!value) {
+  private set sortDir(value: string | null) {
+    if (!value || !this.sort) {
       this._sortDir = null
       return
     }
-    const dir = value.toLowerCase()
-    this._sortDir = dir !== 'asc' && dir !== 'desc' ? 'desc' : dir
+    const sort = value.toLowerCase()
+    this._sortDir = sort !== 'asc' && sort !== 'desc' ? 'desc' : sort
   }
 
   get filter(): string | null {
@@ -76,12 +68,7 @@ export class SearchParams {
   }
 
   private set filter(value: string | null) {
-    if (!value) {
-      this._filter = null
-      return
-    }
-    this._filter = value
-    //this._filter = value === null || value === '' ? null : value
+    this._filter = !value ? null : value
   }
 }
 
